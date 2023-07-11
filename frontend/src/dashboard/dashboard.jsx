e6683583497b98d5d3c7a46fddbc93355467419e
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-11 16:19:27 -0300

    frontend - configurando o valueBox e aplicando

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react";  
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render(){
@@ -8,8 +10,15 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title="Dashboard" small='Versão 1.0'/>
                 <Content>
-                    Dashboard
-                </Content>
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank'
+                            value='R$ 10' text='total de créditos'/>
+                        <ValueBox cols='12 4' color='red' icon='credit-card'
+                            value='R$ 10' text='total de Débitos'/> 
+                        <ValueBox cols='12 4' color='blue' icon='money'
+                            value='R$ 10' text='Valor Consolidado'/>      
+                    </Row>
+                </Content>                
             </div>
         )
     }
