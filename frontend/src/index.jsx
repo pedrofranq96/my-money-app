84987739788fb7f92ad31b01cfbb33cdc9856a48
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-13 17:08:07 -0300

    frontend - melhorias de inclusão do bc

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,9 +5,11 @@ import { Provider } from "react-redux";
 import promise from "redux-promise";
 import reducers from './main/reducers'
 import App from "./main/app"
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi,thunk,promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
         <App/>
