2e0fa3817a57eccd156f7798b42c8101e8dc0c24
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-13 16:54:03 -0300

    frontend - exibindo as mensagens de erro/sucesso

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,7 +4,7 @@ import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar';
 import Footer from '../common/template/footer';
 import Routes from './routes';
-
+import Messages from '../common/msg/messages';
 export default props => (
     <div className='skin-blue' style={{minHeight: 0}}>
         <Header/>
@@ -13,6 +13,6 @@ export default props => (
             <Routes/>
         </div>
         <Footer />
-        
+        <Messages/>
     </div>
 )
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -3,12 +3,13 @@ import { reducer as formReducer } from "redux-form"
 import DashboardReducer from "../dashboard/dashboardReducer";
 import TabReducer from "../common/tab/tabReducer";
 import BillingCycleReducer from "../billingCycle/billingCycleReducer";
-
+import {reducer as toastrReducer} from "react-redux-toastr";
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
-    form: formReducer
+    form: formReducer,
+    toastr: toastrReducer
 })
 
 export default rootReducer
