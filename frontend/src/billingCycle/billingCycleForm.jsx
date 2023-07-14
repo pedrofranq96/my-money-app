600c91cbda7cbed7a0735137ae11b182a7bdc0df
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-14 16:01:27 -0300

    frontend - generalizando o componenet creditList

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { init } from './billingCycleActions'
 import { reduxForm, Field, formValueSelector } from "redux-form"
 import LabelAndInput from "../common/form/labelAndInput"
-import CreditList from "./creditList";
+import ItemList from "./itemList";
 
 class BillingCycleForm extends Component{
     render(){
@@ -18,7 +18,8 @@ class BillingCycleForm extends Component{
                         label='Mês:' cols='12 4' placeholder='Informe o mês...'/>
                     <Field name='year' component={LabelAndInput} type='number' readOnly={readOnly}
                         label='Ano:' cols='12 4' placeholder='Informe o ano...'/>
-                    <CreditList cols='12 6' list={credits} readOnly={readOnly}/>
+                    <ItemList cols='12 6' list={credits} readOnly={readOnly} 
+                        field='credits' legend='Créditos:'/>
                 </div>
                 <div className="box-footer">
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>
