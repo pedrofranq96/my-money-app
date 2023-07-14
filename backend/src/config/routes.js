b0c29c68241612703b459bff6d2ce22065c77a3a
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-14 18:16:13 -0300

    backend - configurando as rotas e sua visibilidade

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const auth = require('./auth')
 
-module.exports = function(server){
-    //URL base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router)
-    // Rotas de ciclo de pagamento
+module.exports = function(server){ 
+    //rotas protegidaas por token JWT
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
     const BillingCicle = require('../api/billingCycle/billingCycleService')
     BillingCicle.register(router, '/billingCycles')
+    
+    //rotas abertas
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
