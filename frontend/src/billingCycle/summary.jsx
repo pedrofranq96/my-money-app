1575b102bc58ff94216eec201ec15747bc863834
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-17 19:53:52 -0300

    Atualizando as libs - finalizando a aplicação

--- frontend/src/billingCycle/summary.jsx
+++ frontend/src/billingCycle/summary.jsx
@@ -1,7 +1,7 @@
 import React,{Component} from "react";
 import Grid from "../common/layout/grid";
 import Row from "../common/layout/row";
-import ValueBox from "../common/widget/valuebox";
+import ValueBox from "../common/widget/valueBox"
 
 export default({credit, debt}) => (
     <Grid cols='12'>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,19 @@ import { applyMiddleware,createStore } from "redux";
 import { Provider } from "react-redux";
 import promise from "redux-promise";
 import reducers from './main/reducers'
-import Routes from "./main/routes"
+import AuthOApp from "./main/authOApp";
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
+//import reportWebVitals from './reportWebVitals';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = applyMiddleware(multi,thunk,promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
-        <Routes/>
+        <AuthOApp/>
     </Provider>
 , document.getElementById('app'))
+
+
+
+//reportWebVitals();
